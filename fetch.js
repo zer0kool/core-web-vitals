ae98a6ee4cd89b93230984eff2c7f09e2ce712da
zer0kool <zer0kool@users.noreply.github.com>
2020-06-28 20:47:30 -0700

    card update

--- fetch.js
+++ fetch.js
@@ -1,6 +1,5 @@
 const CrUXApiUtil = {};
 CrUXApiUtil.KEY = 'AIzaSyBX3phN4IuKNtJkQrTeAL0lZL95kHUY08o';
-
 const endpointUrl = 'https://chromeuxreport.googleapis.com/v1/records:queryRecord';
 var url = `${endpointUrl}?key=${CrUXApiUtil.KEY}`;
 var app = document.querySelector('#app');
@@ -15,54 +14,69 @@ document.querySelector('form').addEventListener('submit', function (e) {
     if (app.contains(child)) {
         M.toast({ html: toastHTML });
     } else {
-        M.toast({
-            html: `Building a request to the API.`,
-            classes: 'green',
-            displayLength: 600
-        })
+        M.toast({html: `Building a request to the API.`, classes: 'green', displayLength: 600})
         getData(origin);
     }
 })
 
 CrUXApiUtil.query = async function (requestBody) {
-    const resp = await fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(requestBody),
-    });
+    const resp = await fetch(url, {method: 'POST', body: JSON.stringify(requestBody)});
     const json = await resp.json();
     if (resp.ok) { return json; }
-    M.toast({
-        html: `${json.error.message}`,
-        classes: 'red darken-4 white-text'
-    })
+    M.toast({ html: `${json.error.message}`, classes: 'red darken-4 white-text'})
     throw new Error(json.error.message);
 };
 
 getData = async (origin) => {
-    const json = await CrUXApiUtil.query({ origin: `https://${origin}/` });
-    console.log('CrUX API response:', json);
-    const favicon  = `https://${origin}/favicon.ico`
-    const labeledMetrics = labelMetricData(json.record.metrics);
-    const data = buildCard(labeledMetrics, origin, favicon)
+    const sum = await CrUXApiUtil.query({ origin: `https://${origin}/`});
+    const phone = await CrUXApiUtil.query({ origin: `https://${origin}/`, formFactor: "PHONE"});
+    const desktop = await CrUXApiUtil.query({ origin: `https://${origin}/`, formFactor: "DESKTOP"});
+    const sumMetrics = labelMetricData(sum.record.metrics, sum.record.key.formFactor);
+    const phoneMetrics = labelMetricData(phone.record.metrics, phone.record.key.formFactor);
+    const desktopMetrics = labelMetricData(desktop.record.metrics, desktop.record.key.formFactor);
+    const labeledMetrics = [sumMetrics, phoneMetrics, desktopMetrics]
+    const data = buildCard(labeledMetrics, origin);
 }
 
-function buildCard(labeledMetrics, origin, favicon) {
-    var siteName = origin.replace(/^www\./, '').split('.').slice(0, -1).join('.');
-
+function buildCard(labeledMetrics, origin) {
+    const favicon  = `https://${origin}/favicon.ico`
+    let siteName = origin.replace(/^www\./, '').split('.').slice(0, -1).join('.');
+    let sumId = `${siteName}SUM`
+    let phoneId = `${siteName}PHONE`
+    let desktopId = `${siteName}DESKTOP`
     let card = `
         <div class="card" id="${siteName}"><div class="cardHeader">
             <img src="${favicon}">
             <span>${siteName}</span>
         </div>
-    `
-    document.getElementById('app').insertAdjacentHTML("beforeend", card);
-    labeledMetrics.forEach( metric => {
-        const finalData = { acronym:"", good:"", ok:"", poor:""}
+        <div id="cardBody" class="row">
+        <div class="col s12">
+            <ul class="tabs">
+                <li class="tab col s3"><a href="#${sumId}">Sum</a></li>
+                <li class="tab col s3"><a class="active" href="#${phoneId}">Mobile</a></li>
+                <li class="tab col s3"><a href="#${desktopId}">Desktop</a></li>
+            </ul>
+        </div>
+        <div id="${sumId}" class="col s12"></div>
+        <div id="${phoneId}" class="col s12"></div>
+        <div id="${desktopId}" class="col s12"></div>
+        </div>
+    `; document.getElementById('app').insertAdjacentHTML("afterbegin", card);
+    labeledMetrics.forEach ( formFactor => {
+        buildData(formFactor, siteName);
+    })
+    var el = document.querySelectorAll('.tabs');
+    var instance = M.Tabs.init(el, {});
+}
+
+function buildData(labeledMetrics, siteName) {
+        labeledMetrics.forEach( metric => {
+        const finalData = {key:"", acronym:"", good:"", ok:"", poor:""}
+        finalData.key = siteName + metric.key;
         finalData.acronym = metric.acronym;
         finalData.good = metric.labeledBins[0].percentage.toFixed(2);
         finalData.ok = metric.labeledBins[1].percentage.toFixed(2);
         finalData.poor = metric.labeledBins[2].percentage.toFixed(2);
-
         let htmlBar = `
         <section class="${finalData.acronym}">
         <h2>${finalData.acronym}</h2>
@@ -72,12 +86,13 @@ function buildCard(labeledMetrics, origin, favicon) {
                 <div class="box-needs-improvement" data-title="${finalData.ok}% needs improvement"></div>
                 <div class="box-poor" data-title="${finalData.poor}% poor"></div>
         </section>
-        `
-        document.querySelector(`#${siteName}`).insertAdjacentHTML("beforeend", htmlBar);
+        `; document.querySelector(`#${finalData.key}`).insertAdjacentHTML("beforeend", htmlBar);
     });
 }
 
-function labelMetricData(metrics) {
+function labelMetricData(metrics, key) {
+    if(key === undefined){key = "SUM"}
+    console.log(key)
     const nameToAcronymMap = {
         first_contentful_paint: 'FCP',
         largest_contentful_paint: 'LCP',
@@ -95,6 +110,7 @@ function labelMetricData(metrics) {
             };
         });
         return {
+            key: key,
             acronym: nameToAcronymMap[metricName],
             name: metricName,
             labeledBins,
