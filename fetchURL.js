a6a295d8f635c0eed6617d84d3664567c2da64a9
Alejandro Loaiza <zer0kool@users.noreply.github.com>
2021-04-08 10:53:50 -0700

    Key updated

--- fetchURL.js
+++ fetchURL.js
@@ -1,5 +1,5 @@
 const CrUXApiUtil = {};
-CrUXApiUtil.KEY = 'AIzaSyBX3phN4IuKNtJkQrTeAL0lZL95kHUY08o';
+CrUXApiUtil.KEY = 'AIzaSyCv-IHj-oddickRMsoI5UBAJx3Cwj-mwck';
 const endpointUrl = 'https://chromeuxreport.googleapis.com/v1/records:queryRecord';
 var url = `${endpointUrl}?key=${CrUXApiUtil.KEY}`;
 var appUrl = document.querySelector('#cruxurl #app');
