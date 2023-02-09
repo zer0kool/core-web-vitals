32d353bfd74d68260758a6b4e0e183b322b92394
zer0kool <zer0kool@users.noreply.github.com>
2023-02-08 18:01:35 -0800

    Changes to be committed: 	modified:   fetchURL.js

--- fetchURL.js
+++ fetchURL.js
@@ -77,64 +77,76 @@ function buildUrlCardData(labeledMetrics, origin, pageType, network, dates) {
     let date = Date(); 
 		if (network === "default") {
 			let card = `
-					<div class="card urlData" id="${siteName}">
-							<i class="activator material-icons right">call_to_action</i>
-							<div class="cardHeader">
-									<img class="activator" aria-label="${siteName} logo" src="${favicon}">
-									<span data-title="${origin}">${cardTitle}</span>
-							</div>
-							<div id="cardBody">
-								<div id="network" data-type="${network}" class="${network} row active">
-								<span class="networkType">effective connection type is unspecified</span>
-										<div class="col s12">
-												<ul class="tabs">
-														<li class="tab col s3"><a href="#${sumId}">Sum</a></li>
-														<li class="tab col s3"><a class="active" href="#${phoneId}">Mobile</a></li>
-														<li class="tab col s3"><a href="#${desktopId}">Desktop</a></li>
-														<li class="tab col s3"><a href="#${tabletId}">Tablet</a></li>
-												</ul>
-										</div>
-										<div id="${sumId}" class="col s12"><div class="metrics"></div></div>
-										<div id="${phoneId}" class="col s12"><div class="metrics"></div></div>
-										<div id="${desktopId}" class="col s12"><div class="metrics"></div></div>
-										<div id="${tabletId}" class="col s12"><div class="metrics"></div></div>
-								</div>
-								<span class="date">
-								collection period: ${dates.first.month}-${dates.first.day}-${dates.first.year} to ${dates.last.month}-${dates.last.day}-${dates.last.year}
-								</span>
-							</div>
-					<div class="card-reveal">
-						<span class="close">remove card</span>
-						<span class="card-title grey-text text-darken-4">Crux settings<i class="material-icons right">close</i></span>
-						<p>If the effective connection type is unspecified, then aggregated data over all effective connection types will be returned.</p>
-
-						<div class="periodDate"><p>
-						The aggregated data on this card is from </p>
-							<span class="firstDate">${dates.first.month}-${dates.first.day}-${dates.first.year}</span> to
-							<span class="lastDate">${dates.last.month}-${dates.last.day}-${dates.last.year}</span>
+			<div class="card urlData" id="${siteName}">
+				<i class="activator material-icons right">call_to_action</i>
+				<div class="cardHeader">
+					<img class="activator" aria-label="${siteName} logo" src="${favicon}">
+					<span data-title="${origin}">${cardTitle}</span>
+				</div>
+				<div id="cardBody">
+					<div id="network" data-type="${network}" class="${network} row active">
+						<span class="networkType">effective connection type is unspecified</span>
+						<div class="col s12">
+							<ul class="tabs">
+								<li class="tab col s3"><a href="#${sumId}">Sum</a></li>
+								<li class="tab col s3"><a class="active" href="#${phoneId}">Mobile</a></li>
+								<li class="tab col s3"><a href="#${desktopId}">Desktop</a></li>
+								<li class="tab col s3"><a href="#${tabletId}">Tablet</a></li>
+							</ul>
 						</div>
-
-						<!-- 3G connectivity -->
-						<p>Filter 3G connection bucket</p>
-						<div class="switch n3g">
-							<label>
-								Off
-								<input data-card="${siteName}" type="checkbox">
-								<span class="lever"></span>
-								On
-							</label>
+						<div id="${sumId}" class="col s12">
+							<div class="metrics"></div>
 						</div>
-						<!-- 4G connectivity -->
-						<p>Filter 4G connection bucket</p>
-						<div class="switch n4g">
-							<label>
-								Off
-								<input data-card="${siteName}" type="checkbox">
-								<span class="lever"></span>
-								On
-							</label>
+						<div id="${phoneId}" class="col s12">
+							<div class="metrics"></div>
+						</div>
+						<div id="${desktopId}" class="col s12">
+							<div class="metrics"></div>
+						</div>
+						<div id="${tabletId}" class="col s12">
+							<div class="metrics"></div>
 						</div>
 					</div>
+					<span class="date">
+						collection period: ${dates.first.month}-${dates.first.day}-${dates.first.year} to
+						${dates.last.month}-${dates.last.day}-${dates.last.year}
+					</span>
+				</div>
+				<div class="card-reveal">
+					<span class="close">remove card</span>
+					<span class="card-title grey-text text-darken-4">Crux settings<i class="material-icons right">close</i></span>
+					<p>If the effective connection type is unspecified, then aggregated data over all effective connection types
+						will be returned.</p>
+			
+					<div class="periodDate">
+						<p>
+							The aggregated data on this card is from </p>
+						<span class="firstDate">${dates.first.month}-${dates.first.day}-${dates.first.year}</span> to
+						<span class="lastDate">${dates.last.month}-${dates.last.day}-${dates.last.year}</span>
+					</div>
+			
+					<!-- 3G connectivity -->
+					<p>Filter 3G connection bucket</p>
+					<div class="switch n3g">
+						<label>
+							Off
+							<input data-card="${siteName}" type="checkbox">
+							<span class="lever"></span>
+							On
+						</label>
+					</div>
+					<!-- 4G connectivity -->
+					<p>Filter 4G connection bucket</p>
+					<div class="switch n4g">
+						<label>
+							Off
+							<input data-card="${siteName}" type="checkbox">
+							<span class="lever"></span>
+							On
+						</label>
+					</div>
+				</div>
+			</div>
 			`;
 
 			document.querySelector('#cruxurl #app').insertAdjacentHTML("afterbegin", card);
@@ -145,22 +157,30 @@ function buildUrlCardData(labeledMetrics, origin, pageType, network, dates) {
 
 		}else{
 			let networkDATA = `
-								<div id="network"  data-type="${network}" class="${network} row active">
-								<span class="networkType">effective connection type is ${network}</span>
-										<div class="col s12">
-												<ul class="tabs">
-														<li class="tab col s3"><a href="#${sumId}">Sum</a></li>
-														<li class="tab col s3"><a class="active" href="#${phoneId}">Mobile</a></li>
-														<li class="tab col s3"><a href="#${desktopId}">Desktop</a></li>
-														<li class="tab col s3"><a href="#${tabletId}">Tablet</a></li>
-												</ul>
-										</div>
-										<div id="${sumId}" class="col s12"><div class="metrics"></div></div>
-										<div id="${phoneId}" class="col s12"><div class="metrics"></div></div>
-										<div id="${desktopId}" class="col s12"><div class="metrics"></div></div>
-										<div id="${tabletId}" class="col s12"><div class="metrics"></div></div>
-								</div>
-							`;
+			<div id="network" data-type="${network}" class="${network} row active">
+				<span class="networkType">effective connection type is ${network}</span>
+				<div class="col s12">
+					<ul class="tabs">
+						<li class="tab col s3"><a href="#${sumId}">Sum</a></li>
+						<li class="tab col s3"><a class="active" href="#${phoneId}">Mobile</a></li>
+						<li class="tab col s3"><a href="#${desktopId}">Desktop</a></li>
+						<li class="tab col s3"><a href="#${tabletId}">Tablet</a></li>
+					</ul>
+				</div>
+				<div id="${sumId}" class="col s12">
+					<div class="metrics"></div>
+				</div>
+				<div id="${phoneId}" class="col s12">
+					<div class="metrics"></div>
+				</div>
+				<div id="${desktopId}" class="col s12">
+					<div class="metrics"></div>
+				</div>
+				<div id="${tabletId}" class="col s12">
+					<div class="metrics"></div>
+				</div>
+			</div>
+			`;
 			document.querySelector(`#cruxurl #${siteName} #cardBody`).insertAdjacentHTML("afterbegin", networkDATA);
 		}
     labeledMetrics.forEach ( formFactor => {
@@ -285,18 +305,22 @@ function buildURLData(labeledMetrics, siteName, network) {
         finalData.ok = metric.labeledBins[1].percentage.toFixed(2);
         finalData.poor = metric.labeledBins[2].percentage.toFixed(2);
         let htmlBar = `
-        <section class="${finalData.acronym}">
-        <div class="labels">
-                <span class="good"><i class="material-icons">sentiment_very_satisfied</i>${finalData.good}%</span>
-                <span class="ok"><i class="material-icons">sentiment_neutral</i>${finalData.ok}%</span>
-                <span class="poor"><i class="material-icons">sentiment_very_dissatisfied</i>${finalData.poor}%</span>
-        </div>
-        <div class="flex"><h2>${finalData.name}</h2>
-        <div class="grid-container" style="grid-template-columns: ${finalData.good}% ${finalData.ok}% ${finalData.poor}%;">
-                <div class="box-good" data-title="${finalData.good}% good"></div>
-                <div class="box-needs-improvement" data-title="${finalData.ok}% needs improvement"></div>
-                <div class="box-poor" data-title="${finalData.poor}% poor"></div>
-        </section> </div>
+		<section class="${finalData.acronym}">
+			<div class="labels">
+				<span class="good"><i class="material-icons">sentiment_very_satisfied</i>${finalData.good}%</span>
+				<span class="ok"><i class="material-icons">sentiment_neutral</i>${finalData.ok}%</span>
+				<span class="poor"><i class="material-icons">sentiment_very_dissatisfied</i>${finalData.poor}%</span>
+			</div>
+			<div class="flex">
+				<h2>${finalData.name}</h2>
+				<div class="grid-container"
+					style="grid-template-columns: ${finalData.good}% ${finalData.ok}% ${finalData.poor}%;">
+					<div class="box-good" data-title="${finalData.good}% good"></div>
+					<div class="box-needs-improvement" data-title="${finalData.ok}% needs improvement"></div>
+					<div class="box-poor" data-title="${finalData.poor}% poor"></div>
+				</div>
+			</div>
+		</section>
         `;document.querySelector(`#cruxurl #${finalData.key} .metrics`).insertAdjacentHTML("beforeend", htmlBar);
     });
 }
